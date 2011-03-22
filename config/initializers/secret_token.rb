b76f70ad4b0b17a0a4d65291ac65af950c18d011
Michal Niemczyk <berd.one@gmail.com>
2011-03-22 21:50:11 +0100

    proba druga

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZtwRor::Application.config.secret_token = '7ee41b09ce10a15f592418c746549ba70f689dfd746b0573a0c59ad7deccfb6256942aec88973c15a40c7e41ed384be0b6e808436451df9b5170d88c53947dea'
